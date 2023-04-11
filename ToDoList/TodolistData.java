de534ad883402f2d0bcc182db7fa124aec9cf597
DeepikaMorla <124659419+DeepikaMorla@users.noreply.github.com>
2023-04-11 12:14:12 +0530

    Create TodolistData.java

---
+++ ToDoList/TodolistData.java
@@ -0,0 +1,17 @@
+class TodolistData
+  {
+    String task_name;
+   
+    public TodolistData(String tn){
+      task_name = tn;
+     
+    }
+    public String getTask_name(){
+      return task_name;
+    }
+    public String toString()
+    {
+      return task_name;
+    }
+   
+  }

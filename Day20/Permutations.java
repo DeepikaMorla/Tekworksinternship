380bb6625a58595e6d1990c8321183353bf4e1f5
DeepikaMorla <124659419+DeepikaMorla@users.noreply.github.com>
2023-04-11 10:54:52 +0530

    Create Permutations.java

---
+++ Day20/Permutations.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Permutations
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter string:");
+      String str=sc.nextLine();
+      displayPermutation(str);
+    }
+    static void displayPermutation(String str)
+    {
+      
+    }
+  }
